28d51270eac278d4d234ee1b04ce827ad8c8ebee
Himanshu Hota <Himanshuhota55@gmail.com>
2022-10-21 15:22:27 +0530

    Added Cart Page

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 const burgerMenu = document.querySelector('#bar');
 const navbar = document.querySelector('#navbar');
 
+
 burgerMenu.addEventListener('click', () => {
     navbar.classList.toggle('activate');
 })
@@ -29,10 +30,24 @@ const onClick = (event) => {
         case 'd-img-4':
             mainImg.src = event.target.src;
             break;
+        case 'd1':
+            const p1 = document.querySelector('#p1')
+            p1.remove();
+            break;
+        case 'd2':
+            const p2 = document.querySelector('#p2')
+            p2.remove();
+            break;
+        case 'd3':
+            const p3 = document.querySelector('#p3')
+            p3.remove();
+            break;
     }
 }
 
 window.addEventListener('click', onClick);
+
+
 
 
 
